8f0c8a7a8622980cd4361b98e3fd316bf24223b4
Samuel Lassoie <samuel.lassoie+1@gmail.com>
2020-10-13 18:31:42 +0200

    listener keydown

--- car/main.js
+++ car/main.js
@@ -5,3 +5,9 @@ setInterval(function()  {
     car.style.top = i % 2 === 0 ? '175px' : '174px'; 
     i++;
 }, 100);
+
+
+document.addEventListener('keydown', function(event){
+    console.log('press clavier');
+    console.log(event);
+});
